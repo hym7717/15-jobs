aab75663eeecdaaead40a430a666c78d1da66428
hym7717 <hy771777@gmail.com>
2026-05-12 15:44:51 +0900

    0512 15:43

--- scrapper.py
+++ scrapper.py
@@ -1,22 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
 
-keyword="파이썬"
-url = f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}&startno=0"
+def search_incruit(keyword, pages):
+
+    
+    jobs = []
+    for i in range(pages):
+        page = i * 30
+
+        url = f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}&startno={page}"
+
+        response = requests.get(url)
+
+        soup = BeautifulSoup(response.text, "html.parser")
+        lis = soup.find_all("li", class_="c_col")
+
+
+
+        for li in lis:
+            company = li.find ("a", class_="cpname").text.strip()
+            title =li.find("div", class_="cell_mid").find("div", class_="cl_top").find("a").text.strip()
+            location = li.find("div", class_="cl_md").find_all("span")[0].text.strip()
+            link = li.find("div", class_="cell_mid").find("div", class_="cl_top").find("a").get("href")
+
+            job_data = {
+                "company" : company,
+                "title" : title,
+                "location" : location,
+                "link" : link
+            }
+
+            jobs.append(job_data)
+
+
+        return jobs
+    
+
+keyword = "파이썬"
+url = f"https://www.jobkorea.co.kr/Search?stext={keyword}&tabType=recruit&Page_No=1"
+
 response = requests.get(url)
-# print(response.text)
-
-soup = BeautifulSoup(response.text, "html.parser")
-
-lis = soup.find_all("li", class_="c_col")
-# print(len(lis))
-# print(lis[0])
 
 
-
-for li in lis[0:1]:
-    # company = li.find ("a", class_="cpname").text.strip()
-    # title =li.find("div", class_="cell_mid").find("div", class_="cl_top").find("a").text.strip()
-    # location = li.find("div", class_="cl_md").find_all("span")[1].text.strip()
-    link = li.find("div", class_="cell_mid").find("div", class_="cl_top").find("a").get("href")
-    print(link)
